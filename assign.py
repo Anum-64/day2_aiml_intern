c3664be318371280fc325af458b6ea9dbbabfe4d
Anum-64 <46nuha64@gmail.com>
2026-02-03 16:36:29 +0530

    sending to the git

---
+++ assign.py
@@ -0,0 +1,7 @@
+name = input("what is your name?")
+current_age = input("what is your current age?")
+current_age = int(current_age)
+age_in_2030 = current_age + 4
+print(f"hey {name},you will be {age_in_2030} years old in 2030!")
+
+
---
+++ assign2.py
@@ -0,0 +1,7 @@
+bill =float(input("Bill:"))
+people = int(input("people:"))
+share = bill / people 
+print("Total Bill:",bill,".Each person pays:",share)
+print(type(bill))
+print(type(people))
+print(type(share))
